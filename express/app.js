57ca3bea065ab46fd4c60f1fdad8cb9ec6a08139
Parvathi <pava@uw.edu>
2020-07-15 14:45:55 -0700

    styling

--- express/app.js
+++ express/app.js
@@ -4,7 +4,8 @@ const app=express();
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 const bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use('/', (req, res, next)=> {
